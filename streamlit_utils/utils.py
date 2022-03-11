5e45d30b216e335aa3f1cf42aa4555a7aa88ba59
jion-kozono <zeon66190329@gmail.com>
2022-03-11 14:52:59 +0900

    set page config

--- streamlit_utils/utils.py
+++ streamlit_utils/utils.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
 def init():
+    st.set_page_config(
+        page_title="英単語テスト",
+        page_icon="📕",
+        initial_sidebar_state="expanded",
+    )
     if 'page' not in st.session_state:
         st.session_state.page = 'create_test'
         st.session_state.words = []
